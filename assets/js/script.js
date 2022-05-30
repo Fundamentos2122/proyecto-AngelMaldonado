9fe263c5d1bfa7008f954cb3830ccf5c8d86fc70
AngelMaldonado <amaldonadojuarez@gmail.com>
2022-05-30 11:50:50 -0500

    Implementación de diseño y arquitectura #1: 📝Calificador de estrellas

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,12 @@
  *  Created On : Mon May 23 2022
  *  File : script.js
  *******************************************/
+
+function cargaScript(url) {
+    var head = document.getElementsByTagName('head')[0];
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = url;
+
+    head.appendChild(script);
+}
